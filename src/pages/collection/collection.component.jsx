17f8bdaa5baa54030855efc6f0d872690acf6770
Adjie <madjiebimaa@gmail.com>
2020-11-25 19:08:38 +0700

    resolve data flow to their spesific collection, edited collection item component, and put it into collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { selectCollection } from "../../redux/shop/shop.selectors";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
+  const { items, title } = collection;
+
   return (
-    <div className="collection">
-      <h1>Adjie</h1>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
